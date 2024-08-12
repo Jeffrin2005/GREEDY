88189f16bdf1b2a5a5bc2c70d83b7798bd54b0a4
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-08-12 21:12:26 +0530

    Minimum Number of Moves to Seat Everyone.cpp

---
+++ Minimum Number of Moves to Seat Everyone.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        // Sort both the seats and students arrays
+       sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+        int totalMoves = 0;
+        for(int i = 0; i < seats.size(); i++){
+            totalMoves += abs(seats[i] - students[i]);
+        }
+        
+        return totalMoves;
+    }
+};

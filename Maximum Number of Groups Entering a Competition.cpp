4eb30399bba98cf0837f130c14646dc84fc142dd
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-08-17 15:35:14 +0530

    Maximum Number of Groups Entering a Competition.cpp

--- Maximum Number of Groups Entering a Competition.cpp
+++ Maximum Number of Groups Entering a Competition.cpp
@@ -17,3 +17,25 @@ public:
         return cnt;
     }
 };
+
+// greedy 
+class Solution {
+public:
+    int maximumGroups(vector<int>& grades) {
+        sort(grades.begin(), grades.end());
+        int prevSum = 0, curSum = 0;
+        int prevCount = 0, curCount = 0, res = 0;
+        for(auto&grade: grades){
+            curSum+=grade;
+            curCount++;
+            if(curCount > prevCount && curSum > prevSum){
+                res++;
+                prevSum = curSum; 
+                prevCount = curCount;
+                curSum =0;
+                 curCount = 0;
+            }
+        }
+        return res;
+    }
+};

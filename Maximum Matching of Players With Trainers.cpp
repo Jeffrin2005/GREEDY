ad6a2c7a3a61a59977970f3d05e4841d32134a6e
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-08-22 22:37:53 +0530

    Create  Maximum Matching of Players With Trainers.cpp

---
+++ Maximum Matching of Players With Trainers.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& g, vector<int>& s){
+        // Sort the greed factors and the sizes of the cookies
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int childIndex = 0; /// pointer for children
+        int cookieIndex = 0; // pointer for   cookiess
+        //iterate through cookies and children
+        while(childIndex < g.size() && cookieIndex < s.size()){
+            if(s[cookieIndex] >= g[childIndex]){
+                // if the current cookie can satisfy the current child
+                childIndex++; // move to the next child
+            }
+            cookieIndex++; // move to the larger next cookie
+        }
+        return childIndex;
+    }
+};
